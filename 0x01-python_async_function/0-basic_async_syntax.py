87d21aead6862acceefaf12ec348ecca04a42bd8
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-08-23 22:04:41 +0200

    basic async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 '''task0.'''
+
 import asyncio
 import random
 
 
-async def async_random(max_dela: float = 10.0) -> float:
-    
-    '''Randomise a sequence of 10 numbers. '''
- delay = random.uniform(0, max_delay) 
-        await asyncio.sleep(1)
-        return delay
-      
+async def wait_random(max_delay: int = 10) -> float:
+    '''wait_random'''
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
