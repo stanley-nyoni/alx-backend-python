c3e9070ec3ece3e8a2f5b412054747ea34e19a68
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-08-23 21:51:53 +0200

    async - corrections

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''task0.'''
+import asyncio
+import random
+
+
+async def async_random(max_dela: float = 10.0) -> float:
+    
+    '''Randomise a sequence of 10 numbers. '''
+ delay = random.uniform(0, max_delay) 
+        await asyncio.sleep(1)
+        return delay
+      
